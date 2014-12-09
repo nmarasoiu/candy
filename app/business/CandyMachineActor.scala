2bbbf4f477ba0b9f361e9d11002d973a82ab4cec
dnmaras <nicolae.marasoiu@gmail.com>
2014-12-09 13:51:16 +0200

    start fixing  proxying; add schedule of a "minimum traffic" so that the expiry can happen the way it is designed now, until i may change it

--- app/business/CandyMachineActor.scala
+++ app/business/CandyMachineActor.scala
@@ -1,49 +1,51 @@
 package business
 
+import java.util.UUID
+import java.util.concurrent.TimeUnit
+
 import akka.actor.{Actor, ActorRef, Props}
 import akka.event.Logging
 import business.dto.{Answer, UserCoin}
 import models.CandyMachineRequest
 import org.joda.time.DateTime
+import play.api.Logger
 
 import scala.collection.immutable.Queue
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration.FiniteDuration
 
 class CandyMachineActor(initialAvailableCandies: Int) extends Actor {
   val log = Logging(context.system, this)
 
-  def start(candies: Int): Actor.Receive =
-    if (candies == 0) noCandies() else equilibrium(candies)
-
   def receive = start(initialAvailableCandies)
+
+  def start(availableCandies: Int): Actor.Receive ={
+    Logger.info("start:"+availableCandies)
+    if (availableCandies == 0) noCandies() else equilibrium(availableCandies)
+  }
 
   def equilibrium(availableCandies: Int): Actor.Receive = {
     case CandyMachineRequest(currentUserId, requestedOperation) =>
+      Logger.info("equilibrium:"+availableCandies)
       requestedOperation match {
         case "coin" =>
           val waitUntil: DateTime = new DateTime().plus(Conf.maxInactiveDuration)
           replace(withCoin(new UserCoin(currentUserId, waitUntil), availableCandies))
+          context.system.scheduler.scheduleOnce(FiniteDuration(Conf.maxInactiveDuration.getSeconds, TimeUnit.SECONDS)) {
+            self ! CandyMachineRequest(UUID.randomUUID().toString, "expireCoinIfNeeded")
+          }
           sender ! Answer.Success
         case "candy" =>
           sender ! Answer.NoCoinsInTheQueue
         case "refill" =>
           replace(equilibrium(1 + availableCandies))
-
+        case "expireCoinIfNeeded" =>
       }
   }
 
-  def restart(newAvailableCandies: Int, reqQueue: Queue[CandyMachineRequest]): Actor.Receive = {
-    if (reqQueue.isEmpty) {
-      start(newAvailableCandies)
-    } else {
-      val newActor = context.actorOf(Props(classOf[CandyMachineActor], newAvailableCandies))
-      for (enqReq <- reqQueue) {
-        newActor forward enqReq
-      }
-      proxy(newActor)
-    }
-  }
   def withCoin(locker: UserCoin, availableCandies: Int, reqQueue: Queue[CandyMachineRequest] = Queue()): Actor.Receive = {
     case req@CandyMachineRequest(currentUserId, requestedOperation) =>
+      Logger.info("withCoin:"+locker+","+availableCandies+","+reqQueue)
       if (currentUserId == locker.userId) {
         requestedOperation match {
           case "candy" =>
@@ -51,11 +53,9 @@ class CandyMachineActor(initialAvailableCandies: Int) extends Actor {
             sender ! Answer.Success
           case "coin" =>
             sender ! Answer.CannotInsertMoreThanOneCoin
-          case "refill" =>
-            replace(withCoin(locker, 1 + availableCandies, reqQueue))
         }
       } else {
-        val newQueue: Queue[CandyMachineRequest] = reqQueue.enqueue(req)
+        val newQueue = reqQueue.enqueue(req)
         if (new DateTime().compareTo(locker.expiryTime) >= 0) {
           replace(restart(availableCandies, newQueue))
         } else {
@@ -64,17 +64,34 @@ class CandyMachineActor(initialAvailableCandies: Int) extends Actor {
       }
   }
 
+  def restart(newAvailableCandies: Int, reqQueue: Queue[CandyMachineRequest]): Actor.Receive = {
+    Logger.info("restart:"+newAvailableCandies+","+reqQueue)
+    if (reqQueue.isEmpty) {
+      start(newAvailableCandies)
+    } else {
+      val newActor = context.actorOf(Props(classOf[CandyMachineActor], newAvailableCandies))
+      for (enqReq <- reqQueue) {
+        if (!"expireCoinIfNeeded".eq(enqReq.operation)) {
+          newActor forward enqReq
+        }
+      }
+      proxy(newActor)
+    }
+  }
+
   def noCandies(): Actor.Receive = {
     case CandyMachineRequest(_, "refill") =>
       replace(start(1))
-    case "coin" | "refill" =>
+    case CandyMachineRequest(_, "coin" | "refill") =>
       sender ! Answer.NoMoreCandies
+    case CandyMachineRequest(_, "expireCoinIfNeeded") =>
   }
 
   //in the current solution we chain proxies whenever there are waiting reqs to ensure that they are going first and then the inbox tail
   //clearly needs rewrite, to have just one proxy step, not a proxy chain which is does not have a bound in memory or processing in the long term
   def proxy(delegate: ActorRef): Actor.Receive = {
     case req =>
+      Logger.info("proxy:"+req)
       delegate forward req
   }
 
--- test/ApplicationSpec.scala
+++ test/ApplicationSpec.scala
@@ -1,9 +1,8 @@
+import org.junit.runner._
 import org.specs2.mutable._
 import org.specs2.runner._
-import org.junit.runner._
-
-import play.api.test._
 import play.api.test.Helpers._
+import play.api.test._
 
 /**
  * Add your spec here.
@@ -12,18 +11,31 @@ import play.api.test.Helpers._
  */
 @RunWith(classOf[JUnitRunner])
 class ApplicationSpec extends Specification {
+  def get(userId: Int, operation: String) = {
+    val Some(result) = route(FakeRequest(GET, "/exec?userId=" + userId + "&operation=" + operation))
+    result
+  }
 
   "Application" should {
 
-    "send 404 on a bad request" in new WithApplication{
+    "send 404 on a bad request" in new WithApplication {
       route(FakeRequest(GET, "/boum")) must beNone
     }
 
-    "render the index page" in new WithApplication{
-      val home = route(FakeRequest(GET, "/")).get
-
-      status(home) must equalTo(OK)
-      contentType(home) must beSome.which(_ == "text/html")
+    "cannot start by asking candy" in new WithApplication() {
+      status(get(23, "candy")) must beEqualTo(BAD_REQUEST)
+      status(get(23,"coin")) must beEqualTo(OK)
+      status(get(23,"coin")) must beEqualTo(BAD_REQUEST)
+      status(get(231,"candy")) must beEqualTo(BAD_REQUEST)
+      status(get(23,"candy")) must beEqualTo(OK)
+      status(get(23,"candy")) must beEqualTo(BAD_REQUEST)
+      status(get(23,"candy")) must beEqualTo(BAD_REQUEST)
+      status(get(23,"candy")) must beEqualTo(BAD_REQUEST)
+      status(get(23,"candy")) must beEqualTo(BAD_REQUEST)
+      status(get(23,"candy")) must beEqualTo(BAD_REQUEST)
+      status(get(23,"candy")) must beEqualTo(BAD_REQUEST)
+      status(get(23,"x")) must beEqualTo(BAD_REQUEST)
     }
+
   }
 }

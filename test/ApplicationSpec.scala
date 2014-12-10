55f3f4b3f50998e9df8fdfd2ecca9ced5335e5f2
dnmaras <nicolae.marasoiu@gmail.com>
2014-12-10 16:11:23 +0200

    make the test cooler

--- test/ApplicationSpec.scala
+++ test/ApplicationSpec.scala
@@ -1,8 +1,13 @@
+import java.util.UUID
+import java.util.concurrent.CountDownLatch
+
 import org.junit.runner._
 import org.specs2.mutable._
 import org.specs2.runner._
 import play.api.test.Helpers._
 import play.api.test._
+
+import scala.concurrent.Future
 
 /**
  * Add your spec here.
@@ -11,7 +16,7 @@ import play.api.test._
  */
 @RunWith(classOf[JUnitRunner])
 class ApplicationSpec extends Specification {
-  def get(userId: Int, operation: String) = {
+  def get(userId: String, operation: String) = {
     val Some(result) = route(FakeRequest(GET, "/exec?userId=" + userId + "&operation=" + operation))
     result
   }
@@ -22,20 +27,32 @@ class ApplicationSpec extends Specification {
       route(FakeRequest(GET, "/boum")) must beNone
     }
 
-    "sir de musturi" in new WithApplication() {
-      status(get(23, "candy")) must beEqualTo(BAD_REQUEST)
-      status(get(23,"coin")) must beEqualTo(OK)
-      status(get(23,"coin")) must beEqualTo(BAD_REQUEST)
-      status(get(23,"candy")) must beEqualTo(OK)
-      status(get(23,"coin")) must beEqualTo(OK)
-      status(get(231,"candy")) must beEqualTo(BAD_REQUEST)
-      status(get(23,"candy")) must beEqualTo(BAD_REQUEST)
-      status(get(23,"candy")) must beEqualTo(BAD_REQUEST)
-      status(get(23,"candy")) must beEqualTo(BAD_REQUEST)
-      status(get(23,"candy")) must beEqualTo(BAD_REQUEST)
-      status(get(23,"candy")) must beEqualTo(BAD_REQUEST)
-      status(get(23,"candy")) must beEqualTo(BAD_REQUEST)
-      status(get(23,"x")) must beEqualTo(BAD_REQUEST)
+    "main testing" in new WithApplication() {
+      val parallelismFactor: Int = 20
+      val cdl = new CountDownLatch(parallelismFactor)
+      for (iteration <- 1 to parallelismFactor) {
+        val B: Int = BAD_REQUEST
+        val O: Int = OK
+        Future {
+          val user = UUID.randomUUID().toString
+          List(status(get(user, "candy")),
+            status(get(user, "coin")),
+            status(get(user, "coin")),
+            status(get(user, "candy")),
+            status(get(user, "coin")),
+            status(get(user, "candy")),
+            status(get(user, "candy")),
+            status(get(user, "coin")),
+            status(get(user, "coin")),
+            status(get(user, "candy")),
+            status(get(user, "candy")),
+            status(get(user, "x")))
+        }.foreach( (result:List[Int]) => {
+          result must beEqualTo(List(B, O, B, O, O, O, B, O, B, O, B, B))
+          cdl.countDown()
+        } )
+      }
+      cdl.await()
     }
 
   }
